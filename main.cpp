fd1559337deeab4e3aa11783502727c6aa538cdb
Retaker <i@retaker.me>
2016-04-26 23:53:52 -0500

    simple test and change

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "bin/plotter.h"
+#include "bin/mario.h"
+#include <wincon.h>
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    Plotter p = Plotter();
-    p.setColor(red);
-    p.plot(2,2,SQUARE);
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    for(int i=0x00;i<=0x0F;i++){
+      SetConsoleTextAttribute(h, i);
+      cout << SQUARE;
+    }
+
     return 0;
 }
